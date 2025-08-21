3bd7b0d8fd4e9fb16aa4472ce69195e7dd4dc5fe
nik <n.a.lyutikov@gmail.com>
2025-08-21 16:41:40 +0300

    HanoiTowers

--- honoiTowers.cpp
+++ honoiTowers.cpp
@@ -4,8 +4,13 @@
 #include <limits>
 #include <ranges>
 
-
+int HanoiTowers(int n, int from, int to) {
+    using namespace std;
+    if (n == 1) return 1;
+    int unusedPeg = 6 - from - to;
+    return HanoiTowers(n - 1, from, unusedPeg) + HanoiTowers(n - 1, unusedPeg, to);
+}
 
 int main() {
-
+    std::cout << HanoiTowers(6, 1, 3);
 }
